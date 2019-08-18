45c07e72286119143d6150d35ec4e2520aca912c
AbhishekH1936 <53899135+AbhishekH1936@users.noreply.github.com>
2019-08-18 15:17:05 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,12 @@
-n=int(input('enter the no of ele'))
-count=0
-#sum=0
-a=0
-b=1
-
-li=[]
-li.append(a)
-li.append(b)
-while count<n-2:
-    summ=a+b
-    li.append(summ)
-    a=b
-    b=summ
-    count+=1
-print('the fibonacci nos are')    
-print(li)    
+def fib(n):
+    if n<0:
+        print('wrong input')
+    elif n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+n=int(input('enter no of eke'))
+for i in range(n):
+    print(fib(i))
