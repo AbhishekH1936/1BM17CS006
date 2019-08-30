37bf9a694ef936b97eb8550c6d0c896b9cde0c4b
AbhishekH1936 <53899135+AbhishekH1936@users.noreply.github.com>
2019-08-30 10:18:20 +0530

    Add files via upload

---
+++ rev1a.py
@@ -0,0 +1,12 @@
+def rev1(str1):
+    str2=str1.split(" ")
+    y=str2[::-1]
+    return y
+def rev2(str2):
+    x=str2[::-1]
+    return x
+b=input('enetr the string')
+x=rev1(b)
+print(x)
+print('charc i reverse order')
+print(rev2(b))
---
+++ search.py
@@ -0,0 +1,32 @@
+def fun(a,n,key):
+    l=0
+    h=n-1
+    f=0
+    while(l<=h):
+        mid=(l+h)//2
+        if(a[mid]==key):
+            f=1
+            break
+        elif(a[mid]>key):
+            h=mid-1
+        else:
+            l=mid+1
+    if(f!=1):
+        return -1
+    return mid
+   
+
+
+l1=[]
+n=int(input("enter the no of elements"))
+for i in range(n):
+   a=int(input("enter  :"))
+   l1.append(a)
+
+key=int(input("enter the no to search :"))
+pos=fun(l1,n,key)
+if(pos!=-1):
+    print("number ",key,"position is  :",pos+1)
+else :
+    print("not found")
+    
