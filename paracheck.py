c949705a2d723a24d8d0e2cc0c3ca5f93bf7aed8
AbhishekH1936 <53899135+AbhishekH1936@users.noreply.github.com>
2019-10-11 09:56:09 +0530

    Update paracheck.py

--- paracheck.py
+++ paracheck.py
@@ -9,8 +9,7 @@ def check(str1):
             return "invalid"
         if len(list1)==0:
            return "valid"
-        else:
-           return "missing format"
+       
 
 b=input('enter the string')
 a=check(b)
